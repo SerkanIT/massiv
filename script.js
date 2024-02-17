2ba09c9dd413e750c81e24e7f6f6d4f39fe85afe
SerkanIT <157528566+SerkanIT@users.noreply.github.com>
2024-02-17 21:13:11 +0500

    push
    string methods

---
+++ script.js
@@ -0,0 +1,46 @@
+let arr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z.",
+];
+
+let names = [];
+names.push("S", "E", "R", "K", "A", "N");
+console.log(names);
+console.log(`${arr[7]}${arr[4]}${arr[11]}${arr[11]}${arr[14]}`);
+
+let mehmon = ["Serkan", "Diyora", "Mavlon", "Rayxona", "Abdullox", "Elif"];
+console.log(
+  `${mehmon[0]}jon ${mehmon[1]}hon ${mehmon[2]}jon ${mehmon[3]}hon ${mehmon[4]}jon ${mehmon[5]}hon`
+);
+
+let texts = ["Men", "boraman", "soat", "8da", "maktabga", "boraman"];
+texts.splice(1, 1, "har", "kuni");
+console.log(texts);
+
+let study = ["I", "study", "JavaScript"];
+study.splice(4, 0, "right", "now");
+console.log(study);
